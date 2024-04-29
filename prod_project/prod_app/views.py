84959cf92038836433f1a740456b479f7dab5795
Anna Levina <anna_levina@internet.ru>
2024-04-29 13:48:20 +0300

    Создано представление для главной страницы

--- prod_project/prod_app/views.py
+++ prod_project/prod_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Main page'
+    }
+    return render(request, 'prod_app/index.html', context)
